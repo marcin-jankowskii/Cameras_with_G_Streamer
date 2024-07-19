1516f76c771cde62a0b7da9b8fbfaff53508b264
marcin-jankowskii <marcinjankowskirep@gmail.com>
2024-07-19 14:58:26 +0200

    v1.1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,6 +28,52 @@ MainWindow::MainWindow(QWidget *parent)
     ui->formatComboBox->setCurrentText("PNG"); // Default to PNG
 
     populateCameraList();
+
+    // Connect sliders to slots
+    connect(ui->focusMinSlider, &QSlider::valueChanged, this, &MainWindow::setFocusMin);
+    connect(ui->zoomSlider, &QSlider::valueChanged, this, &MainWindow::setZoom);
+
+    connect(ui->focusMinSlider2, &QSlider::valueChanged, this, &MainWindow::setFocusMin2);
+    connect(ui->zoomSlider2, &QSlider::valueChanged, this, &MainWindow::setZoom2);
+
+    connect(ui->brightnessSlider, &QSlider::valueChanged, this, &MainWindow::setBrightness);
+    connect(ui->contrastSlider, &QSlider::valueChanged, this, &MainWindow::setContrast);
+    connect(ui->saturationSlider, &QSlider::valueChanged, this, &MainWindow::setSaturation);
+    connect(ui->gainSlider, &QSlider::valueChanged, this, &MainWindow::setGain);
+    connect(ui->sharpnessSlider, &QSlider::valueChanged, this, &MainWindow::setSharpness);
+
+    connect(ui->brightnessSlider2, &QSlider::valueChanged, this, &MainWindow::setBrightness2);
+    connect(ui->contrastSlider2, &QSlider::valueChanged, this, &MainWindow::setContrast2);
+    connect(ui->saturationSlider2, &QSlider::valueChanged, this, &MainWindow::setSaturation2);
+    connect(ui->gainSlider2, &QSlider::valueChanged, this, &MainWindow::setGain2);
+    connect(ui->sharpnessSlider2, &QSlider::valueChanged, this, &MainWindow::setSharpness2);
+
+    // Connect text edits to slots
+    connect(ui->brightnessEdit, &QLineEdit::textChanged, this, &MainWindow::on_brightnessEdit_textChanged);
+    connect(ui->contrastEdit, &QLineEdit::textChanged, this, &MainWindow::on_contrastEdit_textChanged);
+    connect(ui->saturationEdit, &QLineEdit::textChanged, this, &MainWindow::on_saturationEdit_textChanged);
+    connect(ui->gainEdit, &QLineEdit::textChanged, this, &MainWindow::on_gainEdit_textChanged);
+    connect(ui->sharpnessEdit, &QLineEdit::textChanged, this, &MainWindow::on_sharpnessEdit_textChanged);
+
+    connect(ui->brightnessEdit2, &QLineEdit::textChanged, this, &MainWindow::on_brightnessEdit2_textChanged);
+    connect(ui->contrastEdit2, &QLineEdit::textChanged, this, &MainWindow::on_contrastEdit2_textChanged);
+    connect(ui->saturationEdit2, &QLineEdit::textChanged, this, &MainWindow::on_saturationEdit2_textChanged);
+    connect(ui->gainEdit2, &QLineEdit::textChanged, this, &MainWindow::on_gainEdit2_textChanged);
+    connect(ui->sharpnessEdit2, &QLineEdit::textChanged, this, &MainWindow::on_sharpnessEdit2_textChanged);
+
+    // Connect sliders to labels
+    connect(ui->focusMinSlider, &QSlider::valueChanged, this, &MainWindow::updateFocusMinLabel);
+    connect(ui->zoomSlider, &QSlider::valueChanged, this, &MainWindow::updateZoomLabel);
+
+    connect(ui->focusMinSlider2, &QSlider::valueChanged, this, &MainWindow::updateFocusMinLabel2);
+    connect(ui->zoomSlider2, &QSlider::valueChanged, this, &MainWindow::updateZoomLabel2);
+
+    // Set initial label values
+    updateFocusMinLabel(ui->focusMinSlider->value());
+    updateZoomLabel(ui->zoomSlider->value());
+
+    updateFocusMinLabel2(ui->focusMinSlider2->value());
+    updateZoomLabel2(ui->zoomSlider2->value());
 }
 
 MainWindow::~MainWindow()
@@ -114,9 +160,29 @@ void MainWindow::on_startButton_clicked()
         QDir().mkpath(cameraDir);
         qDebug() << "Camera" << i+1 << "save directory:" << cameraDir;
 
+        // Wyłączenie autofocusu, autoeksponowania i ustawienie zoomu
+        QStringList disableAutofocusArgs = {"-d", device, "--set-ctrl=focus_automatic_continuous=0"};
+        QStringList disableAutoExposureArgs = {"-d", device, "--set-ctrl=auto_exposure=1"}; // Manual exposure
+        QStringList setZoomArgs = {"-d", device, QString("--set-ctrl=zoom_absolute=%1").arg(ui->zoomSlider->value())};
+
+        qDebug() << "Executing command:" << "v4l2-ctl" << disableAutofocusArgs;
+        QProcess process1;
+        process1.start("v4l2-ctl", disableAutofocusArgs);
+        process1.waitForFinished();
+
+        qDebug() << "Executing command:" << "v4l2-ctl" << disableAutoExposureArgs;
+        QProcess process2;
+        process2.start("v4l2-ctl", disableAutoExposureArgs);
+        process2.waitForFinished();
+
+        qDebug() << "Executing command:" << "v4l2-ctl" << setZoomArgs;
+        QProcess process3;
+        process3.start("v4l2-ctl", setZoomArgs);
+        process3.waitForFinished();
+
         QWidget* window = new QWidget();
         window->setWindowTitle(QString("Camera %1").arg(i+1));
-        window->resize(640, 480);
+        window->resize(1280, 720); // Set larger window size
         window->show();
         cameraWindows.append(window);
 
@@ -164,4 +230,240 @@ void MainWindow::on_selectDirectoryButton_clicked()
         saveDirectory = dir;
         qDebug() << "Selected save directory:" << saveDirectory;
     }
+}
+
+void MainWindow::setFocusMin(int value)
+{
+    QString device = ui->camera1ComboBox->currentText();
+    QStringList disableAutofocusArgs = {"-d", device, "--set-ctrl=focus_automatic_continuous=0"};
+    QStringList setFocusArgs = {"-d", device, QString("--set-ctrl=focus_absolute=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << disableAutofocusArgs;
+    QProcess process1;
+    process1.start("v4l2-ctl", disableAutofocusArgs);
+    process1.waitForFinished();
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setFocusArgs;
+    QProcess process2;
+    process2.start("v4l2-ctl", setFocusArgs);
+    process2.waitForFinished();
+}
+
+void MainWindow::setFocusMin2(int value)
+{
+    QString device = ui->camera2ComboBox->currentText();
+    QStringList disableAutofocusArgs = {"-d", device, "--set-ctrl=focus_automatic_continuous=0"};
+    QStringList setFocusArgs = {"-d", device, QString("--set-ctrl=focus_absolute=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << disableAutofocusArgs;
+    QProcess process1;
+    process1.start("v4l2-ctl", disableAutofocusArgs);
+    process1.waitForFinished();
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setFocusArgs;
+    QProcess process2;
+    process2.start("v4l2-ctl", setFocusArgs);
+    process2.waitForFinished();
+}
+
+void MainWindow::setZoom(int value)
+{
+    QString device = ui->camera1ComboBox->currentText();
+    QStringList setZoomArgs = {"-d", device, QString("--set-ctrl=zoom_absolute=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setZoomArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setZoomArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setZoom2(int value)
+{
+    QString device = ui->camera2ComboBox->currentText();
+    QStringList setZoomArgs = {"-d", device, QString("--set-ctrl=zoom_absolute=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setZoomArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setZoomArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setBrightness(int value)
+{
+    QString device = ui->camera1ComboBox->currentText();
+    QStringList setBrightnessArgs = {"-d", device, QString("--set-ctrl=brightness=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setBrightnessArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setBrightnessArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setContrast(int value)
+{
+    QString device = ui->camera1ComboBox->currentText();
+    QStringList setContrastArgs = {"-d", device, QString("--set-ctrl=contrast=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setContrastArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setContrastArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setSaturation(int value)
+{
+    QString device = ui->camera1ComboBox->currentText();
+    QStringList setSaturationArgs = {"-d", device, QString("--set-ctrl=saturation=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setSaturationArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setSaturationArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setGain(int value)
+{
+    QString device = ui->camera1ComboBox->currentText();
+    QStringList setGainArgs = {"-d", device, QString("--set-ctrl=gain=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setGainArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setGainArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setSharpness(int value)
+{
+    QString device = ui->camera1ComboBox->currentText();
+    QStringList setSharpnessArgs = {"-d", device, QString("--set-ctrl=sharpness=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setSharpnessArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setSharpnessArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setBrightness2(int value)
+{
+    QString device = ui->camera2ComboBox->currentText();
+    QStringList setBrightnessArgs = {"-d", device, QString("--set-ctrl=brightness=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setBrightnessArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setBrightnessArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setContrast2(int value)
+{
+    QString device = ui->camera2ComboBox->currentText();
+    QStringList setContrastArgs = {"-d", device, QString("--set-ctrl=contrast=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setContrastArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setContrastArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setSaturation2(int value)
+{
+    QString device = ui->camera2ComboBox->currentText();
+    QStringList setSaturationArgs = {"-d", device, QString("--set-ctrl=saturation=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setSaturationArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setSaturationArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setGain2(int value)
+{
+    QString device = ui->camera2ComboBox->currentText();
+    QStringList setGainArgs = {"-d", device, QString("--set-ctrl=gain=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setGainArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setGainArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::setSharpness2(int value)
+{
+    QString device = ui->camera2ComboBox->currentText();
+    QStringList setSharpnessArgs = {"-d", device, QString("--set-ctrl=sharpness=%1").arg(value)};
+
+    qDebug() << "Executing command:" << "v4l2-ctl" << setSharpnessArgs;
+    QProcess process;
+    process.start("v4l2-ctl", setSharpnessArgs);
+    process.waitForFinished();
+}
+
+void MainWindow::updateZoomLabel(int value)
+{
+    ui->zoomLabel->setText(QString("Zoom: %1").arg(value));
+}
+
+void MainWindow::updateFocusMinLabel(int value)
+{
+    ui->focusMinLabel->setText(QString("Focus Min: %1").arg(value));
+}
+
+void MainWindow::updateZoomLabel2(int value)
+{
+    ui->zoomLabel2->setText(QString("Zoom: %1").arg(value));
+}
+
+void MainWindow::updateFocusMinLabel2(int value)
+{
+    ui->focusMinLabel2->setText(QString("Focus Min: %1").arg(value));
+}
+
+void MainWindow::on_brightnessEdit_textChanged(const QString &value)
+{
+    ui->brightnessSlider->setValue(value.toInt());
+}
+
+void MainWindow::on_contrastEdit_textChanged(const QString &value)
+{
+    ui->contrastSlider->setValue(value.toInt());
+}
+
+void MainWindow::on_saturationEdit_textChanged(const QString &value)
+{
+    ui->saturationSlider->setValue(value.toInt());
+}
+
+void MainWindow::on_gainEdit_textChanged(const QString &value)
+{
+    ui->gainSlider->setValue(value.toInt());
+}
+
+void MainWindow::on_sharpnessEdit_textChanged(const QString &value)
+{
+    ui->sharpnessSlider->setValue(value.toInt());
+}
+
+void MainWindow::on_brightnessEdit2_textChanged(const QString &value)
+{
+    ui->brightnessSlider2->setValue(value.toInt());
+}
+
+void MainWindow::on_contrastEdit2_textChanged(const QString &value)
+{
+    ui->contrastSlider2->setValue(value.toInt());
+}
+
+void MainWindow::on_saturationEdit2_textChanged(const QString &value)
+{
+    ui->saturationSlider2->setValue(value.toInt());
+}
+
+void MainWindow::on_gainEdit2_textChanged(const QString &value)
+{
+    ui->gainSlider2->setValue(value.toInt());
+}
+
+void MainWindow::on_sharpnessEdit2_textChanged(const QString &value)
+{
+    ui->sharpnessSlider2->setValue(value.toInt());
 }
--- mainwindow.h
+++ mainwindow.h
@@ -11,6 +11,8 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+class CameraThread; // Forward declaration
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -25,14 +27,50 @@ private slots:
     void on_stopRecordingButton_clicked();
     void on_stopButton_clicked();
     void on_selectDirectoryButton_clicked();
-    void populateCameraList();
+
+    void setFocusMin(int value);
+    void setZoom(int value);
+
+    void setFocusMin2(int value); // For camera 2
+    void setZoom2(int value);     // For camera 2
+
+    void setBrightness(int value);
+    void setContrast(int value);
+    void setSaturation(int value);
+    void setGain(int value);
+    void setSharpness(int value);
+
+    void setBrightness2(int value);
+    void setContrast2(int value);
+    void setSaturation2(int value);
+    void setGain2(int value);
+    void setSharpness2(int value);
+
+    void updateFocusMinLabel(int value);
+    void updateZoomLabel(int value);
+
+    void updateFocusMinLabel2(int value); // For camera 2
+    void updateZoomLabel2(int value);     // For camera 2
+
+    void on_brightnessEdit_textChanged(const QString &value);
+    void on_contrastEdit_textChanged(const QString &value);
+    void on_saturationEdit_textChanged(const QString &value);
+    void on_gainEdit_textChanged(const QString &value);
+    void on_sharpnessEdit_textChanged(const QString &value);
+
+    void on_brightnessEdit2_textChanged(const QString &value);
+    void on_contrastEdit2_textChanged(const QString &value);
+    void on_saturationEdit2_textChanged(const QString &value);
+    void on_gainEdit2_textChanged(const QString &value);
+    void on_sharpnessEdit2_textChanged(const QString &value);
 
 private:
     Ui::MainWindow *ui;
-    QList<CameraThread*> cameraThreads;
-    QList<QWidget*> cameraWindows; // Lista okien podglądu kamer
-    QStringList getCameraDevices();
     QString saveDirectory;
+    QList<CameraThread*> cameraThreads;
+    QList<QWidget*> cameraWindows;
+    void populateCameraList();
+    QStringList getCameraDevices();
 };
 
 #endif // MAINWINDOW_H
